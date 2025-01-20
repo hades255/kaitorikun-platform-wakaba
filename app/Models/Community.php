8855bbf0dab6a40e1c3825212f61e39816fd9454
hades255 <hades.255@outlook.com>
2025-01-21 00:31:30 +0900

    udpate database for communities & translate new notifications

---
+++ app/Models/Community.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Community extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['name', 'description', 'icon', 'requireApproval', 'isPublic', 'user_id'];
+
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
+
+    public function limitedPosts($limit = 10, $offset = 0, $sortBy = 'created_at', $sortOrder = 'desc')
+    {
+        return $this->posts()
+            ->orderBy($sortBy, $sortOrder)
+            ->skip($offset)
+            ->take($limit)
+            ->with(['reactions', 'replies'])
+            ->get();
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class, 'channel_users');
+    }
+
+    public function invitations()
+    {
+        return $this->hasMany(Invitation::class);
+    }
+
+    public static function getMyChannels($userId)
+    {
+        return self::where('user_id', $userId)->get();
+    }
+
+    public static function getChannelsForUser($userId)
+    {
+        return self::whereHas('users', function ($query) use ($userId) {
+            $query->where('user_id', $userId);
+        })->get();
+    }
+}

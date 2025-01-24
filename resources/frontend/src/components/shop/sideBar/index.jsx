b93da0302cc5cc2a4d3357743048339483b429fc
hades255 <hades.255@outlook.com>
2025-01-24 15:47:54 +0900

    update calendar tab issue

--- resources/frontend/src/components/shop/sideBar/index.jsx
+++ resources/frontend/src/components/shop/sideBar/index.jsx
@@ -72,9 +72,9 @@ const Sidebar = (props) => {
                     <ChannelSidebar />
                 ) : props.history.location.pathname === "/chat" ? (
                     <ChatSidebar />
-                ) : props.history.location.pathname === '/calendar' 
-                    ? <></> 
-                    : (
+                ) : props.history.location.pathname === "/calendar" ? (
+                    <></>
+                ) : (
                     <nav className="mt-2">
                         <ul
                             className="nav nav-pills nav-sidebar flex-column"
@@ -193,8 +193,7 @@ const TabItem = ({ tab, props }) => {
     const handleClick = () => {
         if (
             !props.history.location.pathname.includes("/communities") &&
-            !props.history.location.pathname.includes("/chat") && 
-            !props.history.location.pathname.includes("/calendar")
+            !props.history.location.pathname.includes("/chat")
         )
             return;
         updateUnreadTab(
@@ -220,7 +219,8 @@ const TabItem = ({ tab, props }) => {
                         !props.history.location.pathname.includes(
                             "/communities"
                         ) &&
-                        !props.history.location.pathname.includes("/chat")),
+                        !props.history.location.pathname.includes("/chat") &&
+                        !props.history.location.pathname.includes("/calendar")),
             })}
             style={{
                 position: "relative",

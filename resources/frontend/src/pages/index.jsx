817247594eeaa6d1620ec9bde9ae388cad775647
hades255 <hades.255@outlook.com>
2025-01-24 07:33:37 +0100

    add calendar page

--- resources/frontend/src/pages/index.jsx
+++ resources/frontend/src/pages/index.jsx
@@ -1,10 +1,12 @@
 import PageNotFound from "./404";
 import { FormControl, DemoTabel } from "./demoPage";
 import Login from "./login";
+import CalendarPage from "./calendar";
 
 export {
     Login,
     PageNotFound,
     FormControl,
-    DemoTabel
+    DemoTabel,
+    CalendarPage
 };
